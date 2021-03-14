6f6fd471bde2de0ea17f1df1ab7881c4c76f3361
Dmitry Ilyin <dima@doty.ru>
2021-03-14 20:33:32 +0300

    Add cJSON to parse internal structures

--- src/vendors/xm.c
+++ src/vendors/xm.c
@@ -1,10 +1,13 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+
+#include "cjson/cJSON.h"
 
 #include "chipid.h"
 #include "tools.h"
@@ -64,7 +67,49 @@ static bool extract_cloud_id() {
     return true;
 }
 
+static void extract_netip_creds(char username[64], char pwd[64]) {
+    FILE *fp = fopen("/mnt/mtd/Config/Account1", "r");
+    if (!fp)
+        return;
+
+    fseek(fp, 0, SEEK_END);
+    size_t len = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    char *config = malloc(len);
+    fread(config, 1, len, fp);
+    fclose(fp);
+    cJSON *json = cJSON_ParseWithLength(config, len);
+    if (json == NULL)
+        goto bailout;
+
+    const cJSON *users = cJSON_GetObjectItemCaseSensitive(json, "Users");
+    if (!cJSON_IsArray(users))
+        goto bailout;
+    const cJSON *user = NULL;
+    cJSON_ArrayForEach(user, users) {
+        const cJSON *name = cJSON_GetObjectItemCaseSensitive(user, "Name");
+        if (!(cJSON_IsString(name) && (name->valuestring != NULL)))
+            goto bailout;
+        const cJSON *password =
+            cJSON_GetObjectItemCaseSensitive(user, "Password");
+        if (!(cJSON_IsString(password) && (password->valuestring != NULL)))
+            goto bailout;
+        strncpy(username, name->valuestring, 64);
+        strncpy(pwd, password->valuestring, 64);
+        break;
+    }
+
+bailout:
+    free(config);
+    cJSON_Delete(json);
+}
+
 void gather_xm_board_info() {
+    char username[64] = {0}, password[64] = {0};
+    extract_netip_creds(username, password);
+    // printf("%s/%s\n", username, password);
+
     detect_xm_product();
     extract_cloud_id();
     detect_nor_chip();
