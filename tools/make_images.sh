8f76d88d43c624e871aef77e543f76f3ba73bc80
Dmitry Ilyin <dima@doty.ru>
2021-06-14 12:58:42 +0300

    Make upgrade bundle from latest ev300

--- tools/make_images.sh
+++ tools/make_images.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-./upgrade_bundle.py --kernel /mnt/noc/uImage.hi3516ev300 --rootfs /mnt/noc/rootfs.squashfs.hi3516ev300 -o up.ev300 --pack -i --cma 'mmz=anonymous,0,0x42000000,96M mmz_allocator=cma'
+PACK=--pack
+
+wget -qO-  \
+  https://github.com/OpenIPC/openipc-2.1/releases/download/latest/openipc.hi3516ev300-br.tgz \
+  | tar xvfz - -C /tmp
+./upgrade_bundle.py \
+  --kernel /tmp/uImage.hi3516ev300 \
+  --rootfs /tmp/rootfs.squashfs.hi3516ev300 \
+  -o upgrade.ev300 $PACK \
+  -i \
+  --cma 'mmz=anonymous,0,0x42000000,96M mmz_allocator=cma'
