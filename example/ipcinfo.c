e847b384c0d4339dbbad591a3f8a308ec1f0eced
Dmitry Ilyin <dima@doty.ru>
2021-06-19 19:42:42 +0300

    Fix 00:12:xx:xx:81:fffffff6 mac output

--- example/ipcinfo.c
+++ example/ipcinfo.c
@@ -69,9 +69,11 @@ static bool try_for_xmmac(int i, size_t size) {
         if (header != 0xd4d2)
             continue;
 
-        printf("%02x:%02x:%02x:%02x:%02x:%02x\n", ptr[0x379 + 0] - 1,
-               ptr[0x37b] - 3, ptr[0x37d] - 5, ptr[0x37f] - 7, ptr[0x381] - 9,
-               ptr[0x383] - 11);
+        uint8_t mac[6] = {ptr[0x379 + 0] - 1, ptr[0x37b] - 3, ptr[0x37d] - 5,
+                          ptr[0x37f] - 7,     ptr[0x381] - 9, ptr[0x383] - 11};
+
+        printf("%02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2],
+               mac[3], mac[4], mac[5]);
         return true;
     }
 
