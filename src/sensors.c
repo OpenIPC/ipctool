8db0341eea56cfed93a58d5adead1649915d65e7
Dmitry Ermakov <de@dimer.org.ua>
2021-09-11 13:34:59 +0300

    Add SC2239,SC4335P,SC401AI,SC501AI (#28)
    Add sc307h, sc2315
    
    Co-authored-by: Dmitry Ilyin <dima@doty.ru>

--- src/sensors.c
+++ src/sensors.c
@@ -287,8 +287,9 @@ static int detect_smartsens_sensor(sensor_ctx_t *ctx, int fd,
         res = 0x1145;
         break;
     case 0x2311:
-        res = 0x2315;
-        break;
+        // XM
+        strcpy(ctx->sensor_id, "SC2315");
+        return true;
     case 0x2330:
         // Untested
         strcpy(ctx->sensor_id, "SC307H");
@@ -307,8 +308,8 @@ static int detect_smartsens_sensor(sensor_ctx_t *ctx, int fd,
         strcpy(ctx->sensor_id, "SC2232H");
         return true;
     case 0xcb10:
-        res = 0x2239;
-        break;
+        strcpy(ctx->sensor_id, "SC2239");
+        return true;
     case 0xcb14:
         // Untested
         res = 0x2335;
@@ -316,6 +317,10 @@ static int detect_smartsens_sensor(sensor_ctx_t *ctx, int fd,
     case 0xcb17:
         res = 0x2232;
         break;
+    case 0xcb1c:
+        // XM
+        strcpy(ctx->sensor_id, "SC307H");
+        return true;
     case 0xcc05:
         // Untested
         strcpy(ctx->sensor_id, "AUGE");
@@ -325,15 +330,15 @@ static int detect_smartsens_sensor(sensor_ctx_t *ctx, int fd,
         strcpy(ctx->sensor_id, "SC3335");
         return true;
     case 0xcd01:
-        // Untested
+        // XM
         strcpy(ctx->sensor_id, "SC4335P");
         return true;
-    case 0xCD2E:
-        // Untested
+    case 0xcd2e:
+        // XM
         strcpy(ctx->sensor_id, "SC401AI");
         return true;
-    case 0xCE1F:
-        // Untested
+    case 0xce1f:
+        // XM
         strcpy(ctx->sensor_id, "SC501AI");
         return true;
     case 0:
