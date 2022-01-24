37a13fcf0e37ed408b90605462ee3abdb2b0f6df
Dmitry Ilyin <dima@doty.ru>
2022-01-24 11:35:14 +0300

    Bump CV300 make_images to OpenIPC 2.2

--- tools/make_images.sh
+++ tools/make_images.sh
@@ -14,14 +14,14 @@ wget -qO-  \
   --cma 'mmz=anonymous,0,0x42000000,96M mmz_allocator=cma'
 
 # CV300
+wget -qO-  \
+  https://github.com/OpenIPC/firmware/releases/download/latest/openipc.hi3516cv300-br.tgz \
+  | tar xvfz - -C /tmp
 wget -P /tmp \
   https://github.com/OpenIPC/chaos_calmer/releases/download/latest/openwrt-hi35xx-16cv300-u-boot.bin
-wget -P /tmp \
-  https://github.com/OpenIPC/chaos_calmer/releases/download/latest/openwrt-hi35xx-16cv300-default-uImage
-wget -P /tmp \
-  https://github.com/OpenIPC/chaos_calmer/releases/download/latest/openwrt-hi35xx-16cv300-default-root.squashfs
 ./upgrade_bundle.py \
   --boot /tmp/openwrt-hi35xx-16cv300-u-boot.bin \
-  --kernel /tmp/openwrt-hi35xx-16cv300-default-uImage \
-  --rootfs /tmp/openwrt-hi35xx-16cv300-default-root.squashfs \
-  -o upgrade.cv300 $PACK
+  --kernel /tmp/uImage.hi3516cv300 \
+  --rootfs /tmp/rootfs.squashfs.hi3516cv300 \
+  -i -o upgrade.cv300 $PACK
+rm /tmp/openwrt-hi35xx-16cv300-u-boot.bin
