5a20cba32c892822ea397a7cecedce1bd27bea20
p0isk <radio.86rk@gmail.com>
2022-02-18 20:28:00 +0300

    Vendor patch (#26)
    * Add chip vendor output parameter for ipcinfo
    
    * Fix usage info for ipcinfo

--- include/ipchw.h
+++ include/ipchw.h
@@ -5,6 +5,8 @@ const char *getchipname();
 const char *getchipfamily();
 const char *getsensoridentity();
 const char *getsensorshort();
+char chip_manufacturer[128];
 float gethwtemp();
+
 
 #endif /* IPCHW_H */
