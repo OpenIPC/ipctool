a6297e523ce84e17427ef0c966dac27aa17ad94c
Dmitry Ermakov <de@dimer.org.ua>
2022-08-11 23:53:17 +0300

    [ingenic] Fix i2c addr for SOI

--- src/hal_ingenic.c
+++ src/hal_ingenic.c
@@ -14,11 +14,11 @@ static unsigned char ssens_addrs[] = {0x60, 0};
 static unsigned char omni_addrs[] = {0x6c, 0};
 static unsigned char onsemi_addrs[] = {0x20, 0};
 static unsigned char gc_addrs[] = {0x6e, 0};
-static unsigned char soi_addrs[] = {0x30, 0x40, 0};
+static unsigned char soi_addrs[] = {0x60, 0x80, 0};
 
 sensor_addr_t ingenic_possible_i2c_addrs[] = {
     {SENSOR_SONY, sony_addrs},     {SENSOR_SMARTSENS, ssens_addrs},
-    {SENSOR_ONSEMI, onsemi_addrs}, {SENSOR_OMNIVISION, omni_addrs},
+    {SENSOR_ONSEMI, onsemi_addrs}, {SENSOR_OM`NIVISION, omni_addrs},
     {SENSOR_GALAXYCORE, gc_addrs}, {SENSOR_SOI, soi_addrs}, {0, NULL}};
 
 typedef unsigned char uint8;
