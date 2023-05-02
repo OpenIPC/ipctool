4a194f123313f07a2782ce781ea309160baf8e9b
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-05-02 15:05:25 +0200

    [Sigmastar] add additional chipset families (#69)
    Co-authored-by: viktorxda <viktorxda@users.noreply.github.com>

--- src/hal/sstar.h
+++ src/hal/sstar.h
@@ -3,10 +3,14 @@
 
 #include <stdbool.h>
 
+#define INFINITY3 0xC2
 #define INFINITY5 0xED
+#define MERCURY5 0xEE
 #define INFINITY6 0xEF
+#define INFINITY2M 0xF0
 #define INFINITY6E 0xF1
 #define INFINITY6B 0xF2
+#define PIONEER3 0xF5
 
 bool mstar_detect_cpu(char *chip_name);
 bool sstar_detect_cpu(char *chip_name);
