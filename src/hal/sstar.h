74b3f09a749b4a24cb00870ae0982cf58547ce49
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-05-04 08:11:00 +0200

    minor refactoring / add sdk codenames (#71)
    * [Sigmastar] add sdk codenames
    
    * minor refactoring
    
    ---------
    
    Co-authored-by: viktorxda <viktorxda@users.noreply.github.com>

--- src/hal/sstar.h
+++ src/hal/sstar.h
@@ -11,14 +11,14 @@
 #define MSTAR_ADDR 0x1F2025A4
 #define SSTAR_ADDR 0x1F003C00
 
-#define INFINITY3  0xC2
-#define INFINITY5  0xED
-#define MERCURY5   0xEE
-#define INFINITY6  0xEF
-#define INFINITY2M 0xF0
-#define INFINITY6E 0xF1
-#define INFINITY6B 0xF2
-#define PIONEER3   0xF5
+#define INFINITY3  0xC2 // Twinkie
+#define INFINITY5  0xED // Pretzel
+#define MERCURY5   0xEE // Mercury5
+#define INFINITY6  0xEF // Macaron
+#define INFINITY2M 0xF0 // Taiyaki
+#define INFINITY6E 0xF1 // Pudding
+#define INFINITY6B 0xF2 // Ispahan
+#define PIONEER3   0xF5 // Ikayaki
 
 bool mstar_detect_cpu(char *chip_name);
 bool sstar_detect_cpu(char *chip_name);
