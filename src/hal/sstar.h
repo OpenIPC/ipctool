99a232a3b7b815100c8b40fda7db37c2e3c6050e
viktorxda <35473052+viktorxda@users.noreply.github.com>
2023-08-01 13:15:32 +0200

    Sigmastar: add missing standalone library directive (#81)
    * Sigmastar: add missing standalone library directive
    
    * Yaml: address compiler warning

--- src/hal/sstar.h
+++ src/hal/sstar.h
@@ -22,7 +22,6 @@
 
 bool mstar_detect_cpu(char *chip_name);
 bool sstar_detect_cpu(char *chip_name);
-unsigned long sstar_totalmem(unsigned long *media_mem);
 void sstar_setup_hal();
 
 #endif /* HAL_SSTAR_H */
