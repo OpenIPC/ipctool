0243cb866429dbef083583148030024a35b89585
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-11-28 00:53:51 +0100

    reginfo: move sigmastar register to separate header

--- src/reginfo.h
+++ src/reginfo.h
@@ -1,7 +1,16 @@
 #ifndef REGINFO_H
 #define REGINFO_H
 
+#include <stdint.h>
 #include <stdlib.h>
+
+typedef const struct {
+    uint32_t address;
+    const char *funcs[];
+} muxctrl_reg_t;
+
+#define MUXCTRL(name, addr, ...)                                               \
+    static muxctrl_reg_t name = {addr, {__VA_ARGS__, 0}};
 
 int reginfo_cmd(int argc, char **argv);
 int gpio_cmd(int argc, char **argv);
