3f882c19b3a8ac2fe0182be837176eb3e4eca74b
viktorxda <35473052+viktorxda@users.noreply.github.com>
2025-08-11 17:55:13 +0200

    Add sc1a4t

--- src/sensors.c
+++ src/sensors.c
@@ -621,6 +621,9 @@ static int detect_smartsens_sensor(sensor_ctx_t *ctx, int fd,
     case 0xdc42:
         res = 0x4336;
         return true;
+    case 0x9a4d:
+        strcpy(ctx->sensor_id, "SC1A4T");
+        return true;
     case 0x9c42:
         strcpy(ctx->sensor_id, "SC4336P");
         return true;
